c900003f080ce05675708760967040b4a1b9426e
TrackRunny <trackrunny@protonmail.com>
2019-07-15 20:40:37 -0700

    Updated git ingore

--- cogs/Information/whois.py
+++ cogs/Information/whois.py
@@ -39,6 +39,7 @@ class Userinfo(commands.Cog):
             embed.add_field(name="• On mobile? ", value="Yes, on mobile!")
         else:
             embed.add_field(name="• On mobile? ", value="Not on a mobile device!")
+        embed.add_field(name="• Status: ", value=f"{member.status}")
         embed.add_field(name="• Top role: ", value=f"`@{member.top_role}`")
         embed.add_field(name="• Roles: ", inline=False,  value=f" ".join([f"`@{role}`, " for role in roles]))
 
