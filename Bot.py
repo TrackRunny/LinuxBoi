4f1bbfc2cbde3a873356090c6f5e196205305198
TrackRunny <trackrunny@protonmail.com>
2019-07-18 13:23:43 -0700

    Updated Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,4 @@
 import os
-
 import discord
 from discord.ext import commands
 
@@ -17,6 +16,8 @@ def read_token():
 
 @client.event
 async def on_ready():
+    await client.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name="Linux videos | l!help"))
     print(f"---------------DiscordBot---------------"
           f"\nBot is online and connected to " + str(client.user) +
           f"\nCreated by TrackRunny#3900 on Discord"
@@ -43,7 +44,7 @@ async def load_information_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> load_information stats`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!load_information stats`")
         await ctx.send(embed=embed)
 
 
@@ -67,7 +68,7 @@ async def unload_information_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> unload_information stats`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!unload_information stats`")
         await ctx.send(embed=embed)
 
 
@@ -91,7 +92,7 @@ async def reload_information_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> reload_information stats`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!reload_information stats`")
         await ctx.send(embed=embed)
 
 
@@ -99,6 +100,86 @@ for filename in os.listdir('./cogs/Information'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.Information.{filename[:-3]}")
 
+# ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+# ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+# ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+@client.command(pass_context=True)
+@commands.is_owner()
+async def load_fun(ctx, extension):
+    client.load_extension(f"cogs.Fun.{extension}")
+    embed = discord.Embed(
+        color=discord.Color.from_rgb(241, 90, 36)
+    )
+    embed.set_author(name="• Fun")
+    embed.add_field(name="Cog command", value=ctx.author.mention + " → One of the Fun cogs has been loaded!")
+    await ctx.send(embed=embed)
+
+
+@load_fun.error
+async def load_fun_error(ctx, error):
+    member = ctx.author
+    if isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            color=discord.Color.from_rgb(241, 90, 36)
+        )
+        embed.set_author(name="• Invalid Argument!")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!load_fun whois`")
+        await ctx.send(embed=embed)
+
+
+@client.command(pass_context=True)
+@commands.is_owner()
+async def unload_fun(ctx, extension):
+    client.unload_extension(f"cogs.Fun.{extension}")
+    embed = discord.Embed(
+        color=discord.Color.from_rgb(241, 90, 36)
+    )
+    embed.set_author(name="• Fun")
+    embed.add_field(name="Cog command", value=ctx.author.mention + " → One of the Fun cogs has been unloaded!")
+    await ctx.send(embed=embed)
+
+
+@unload_fun.error
+async def unload_fun_error(ctx, error):
+    member = ctx.author
+    if isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            color=discord.Color.from_rgb(241, 90, 36)
+        )
+        embed.set_author(name="• Invalid Argument!")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!unload_fun whois`")
+        await ctx.send(embed=embed)
+
+
+@client.command(pass_context=True)
+@commands.is_owner()
+async def reload_fun(ctx, extension):
+    client.reload_extension(f"cogs.Fun.{extension}")
+    embed = discord.Embed(
+        color=discord.Color.from_rgb(241, 90, 36)
+    )
+    embed.set_author(name="• Fun")
+    embed.add_field(name="Cog command", value=ctx.author.mention + " → One of the Fun cogs has been reloaded!")
+    await ctx.send(embed=embed)
+
+
+@reload_fun.error
+async def reload_fun_error(ctx, error):
+    member = ctx.author
+    if isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            color=discord.Color.from_rgb(241, 90, 36)
+        )
+        embed.set_author(name="• Invalid Argument!")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!reload_fun whois`")
+        await ctx.send(embed=embed)
+
+
+for filename in os.listdir('./cogs/Fun'):
+    if filename.endswith('.py'):
+        client.load_extension(f"cogs.Fun.{filename[:-3]}")
 
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -125,7 +206,7 @@ async def load_moderation_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> load_moderation purge`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!load_moderation purge`")
         await ctx.send(embed=embed)
 
 
@@ -149,7 +230,7 @@ async def unload_moderation_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> unload_moderation purge`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!unload_moderation purge`")
         await ctx.send(embed=embed)
 
 
@@ -173,14 +254,13 @@ async def reload_moderation_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> reload_moderation kick`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!reload_moderation kick`")
         await ctx.send(embed=embed)
 
 
 for filename in os.listdir('./cogs/Moderation'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.Moderation.{filename[:-3]}")
-
 
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -207,7 +287,7 @@ async def load_events_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> load_events member`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!load_events member`")
         await ctx.send(embed=embed)
 
 
@@ -231,7 +311,7 @@ async def unload_events_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> unload_events member`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!unload_events member`")
         await ctx.send(embed=embed)
 
 
@@ -255,7 +335,7 @@ async def reload_events_error(ctx, error):
             color=discord.Color.from_rgb(241, 90, 36)
         )
         embed.set_author(name="• Invalid Argument!")
-        embed.add_field(name=member, value="Please put a valid option! Example: `> reload_events member`")
+        embed.add_field(name=member, value="Please put a valid option! Example: `l!reload_events member`")
         await ctx.send(embed=embed)
 
 
@@ -263,38 +343,17 @@ for filename in os.listdir('./cogs/Events'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.Events.{filename[:-3]}")
 
-
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 # ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-
+"""
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send(":facepalm: — Invalid command! Run `> help` to see all commands.")
-
-
-# Work in progress
-
+        await ctx.send(":facepalm: — Invalid command! Run `l!help` to see all commands.")
 """
-@client.command(pass_context=True)
-async def unban(ctx, *, member):
-    embed = discord.Embed(
-        color=discord.Color.from_rgb(241, 90, 36)
-    )
-    sender = ctx.author
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
 
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f' I have unbanned {user.name}#{user.discriminator}')
-            return
-"""
 
 # Work in progress
 
@@ -306,6 +365,7 @@ async def weather(ctx,):
     w = observation.get_weather()
     await ctx.send(f"{w.get_wind()}")
 """
+
 
 # client.run(read_token())
 client.run(read_token(), bot=False)
