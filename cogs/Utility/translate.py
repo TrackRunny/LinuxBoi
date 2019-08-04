00198989177f0788580c4af79131d7a60668c206
TrackRunny <trackrunny@protonmail.com>
2019-08-03 21:15:58 -0700

    Added better free API

--- cogs/Utility/translate.py
+++ cogs/Utility/translate.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from google.cloud import translate
+from mtranslate import translate
 
 
 class Translate(commands.Cog):
@@ -9,54 +9,30 @@ class Translate(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def translate(self, ctx, language, *, text):
-        try:
-            # Instantiates a client
-            translate_client = translate.Client()
+    async def translate(self, ctx, text, *, translation_language):
 
-            # The text to translate
+        language = translate(text, translation_language, 'auto')
 
-            text = text
-            # The target language
-            language = language
+        embed = discord.Embed(
+            color=discord.Color.from_rgb(241, 90, 36)
+         )
+        embed.add_field(name="→ Translation", value='• Your input: `{}`'.format(text)
+                        + "\n• Translated Text: `{}`".format(language)
+                        + "\n• Detected Input Language: `{}`".format(translation_language))
+        embed.add_field(name="→ Supported Languages", inline=False,
+                        value="• Languages: `{}`".format(104))
 
-            # Translates some text into Russian
-            translation = translate_client.translate(text, target_language=language)
-
-            # Shows languages
-            languages = translate_client.get_languages()
-
-            # print(f"Text: {text}")
-            # print(u'Translation: {}'.format(translation['translatedText']))
-            # print("Languages: {}".format(len(languages)))
-
-            embed = discord.Embed(
-                color=discord.Color.from_rgb(241, 90, 36)
-            )
-            embed.add_field(name="→ Translation", value='• Your input: `{}`'.format(text)
-                            + "\n• Translated Text: `{}`".format(translation['translatedText'])
-                            + "\n• Detected Input Language: `{}`".format(translation['detectedSourceLanguage']))
-            embed.add_field(name="→ Supported Languages", inline=False,
-                            value="• Languages: `{}`".format(len(languages)))
-
-            await ctx.send(embed=embed)
-        except Exception:
-            embed = discord.Embed(
-                color=discord.Color.from_rgb(241, 90, 36)
-            )
-            embed.add_field(name="→ Invalid Argument!",
-                            value="• Please put a valid language to be translated to!")
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @translate.error
-    async def unban_error(self, ctx, error):
+    async def translate_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             embed = discord.Embed(
                 color=discord.Color.from_rgb(241, 90, 36)
             )
             embed.add_field(name="→ Invalid Argument!",
-                            value="• Please follow the foramt! Example: `l!translate <language> <message>`"
-                                  "\n• Real World Example: `l!translate en Hola`")
+                            value="• Please put a valid option! Example: `l!translate <\"message\"> <language>`"
+                                  "\n• Please note your message must have quotes around them.")
             await ctx.send(embed=embed)
 
 
