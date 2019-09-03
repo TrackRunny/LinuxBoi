594e2ffd64ed8af61fc6b0d05ad212fa8a85099e
TrackRunny <trackrunny@protonmail.com>
2019-09-02 20:31:01 -0700

    Added Geekjoke command, updated help command

---
+++ cogs/Fun/geekjoke.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import discord
+from discord.ext import commands
+import json
+from random import randint
+
+
+class GeekJoke(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def geekjoke(self, ctx):
+        def random_digits(joke_count):
+            # Return a joke index between first and last joke in data
+            return randint(1, joke_count)
+
+        def get_joke():
+            # Return random joke
+            with open('./cogs/Fun/geekjokes.json', encoding="utf8") as data_file:
+                data = json.load(data_file)
+            joke = data[random_digits(len(data))]
+            return joke
+
+        embed = discord.Embed(
+            color=discord.Color.from_rgb(241, 90, 36)
+        )
+        embed.add_field(name="→ Random Geek joke!", value=f"• {get_joke()}")
+
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(GeekJoke(client))
