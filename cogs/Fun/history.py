cc85c02ef7d27bda3bc4c1653cc88abcdfa4bfc1
TrackRunny <trackrunny@protonmail.com>
2019-10-13 09:40:45 -0700

    Added history command

---
+++ cogs/Fun/history.py
@@ -0,0 +1,26 @@
+import discord
+import aiohttp
+from discord.ext import commands
+
+
+class History(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def history(self, ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://numbersapi.com/random/date?json') as r:
+                res = await r.json()
+                embed = discord.Embed(
+                    color=discord.Color.from_rgb(241, 90, 36)
+                )
+                embed.add_field(name="→ Random History Date", value=f"• {res['text']}"
+                                                                    f"\n• Year: {res['year']}")
+
+                await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(History(client))
