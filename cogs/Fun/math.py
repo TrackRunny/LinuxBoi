5705f2674226b0b9dd4c502c1455e83abbf19b1e
TrackRunny <trackrunny@protonmail.com>
2019-10-17 17:17:51 -0700

    Updated Lavalink Jar + Math command

---
+++ cogs/Fun/math.py
@@ -0,0 +1,26 @@
+import discord
+import aiohttp
+from discord.ext import commands
+
+
+class Math(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def math(self, ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://numbersapi.com/random/math?json') as r:
+                res = await r.json()
+                embed = discord.Embed(
+                    color=discord.Color.from_rgb(241, 90, 36)
+                )
+                embed.add_field(name="→ Random Math Fact", value=f"• Fact: {res['text']}"
+                                                                 f"\n• Number: {res['number']}")
+
+                await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Math(client))
