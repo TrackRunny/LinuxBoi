8c25b92820e2d1346021b632be832e03ee3db079
TrackRunny <trackrunny@protonmail.com>
2019-10-21 17:47:03 -0700

    Added Yo Momma Joke

---
+++ cogs/Fun/yo_mamma_joke.py
@@ -0,0 +1,25 @@
+import discord
+import aiohttp
+from discord.ext import commands
+
+
+class YoMamaJoke(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["momma-joke", "yo-momma-joke"])
+    async def yo_momma_joke(self, ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://api.yomomma.info/') as r:
+                res = await r.json(content_type='text/html')
+                embed = discord.Embed(
+                    color=discord.Color.from_rgb(241, 90, 36)
+                )
+                embed.add_field(name="→ Yo Momma Joke", value=f"• Joke: {res['joke']}")
+
+                await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(YoMamaJoke(client))
