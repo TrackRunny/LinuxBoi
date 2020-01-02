bc4406eb3b95cc1eaf6d7be081fc6363bd8cb11a
TrackRunny <trackrunny@protonmail.com>
2020-01-01 19:30:01 -0800

    Update copy right year

--- LinuxBoi.py
+++ LinuxBoi.py
@@ -2,7 +2,7 @@
 
 """
 LinuxBoi - Discord bot
-Copyright (C) 2019 TrackRunny
+Copyright (C) 2019-2020 TrackRunny
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
