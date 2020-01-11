e4fb659d28e0fb1536e94aee4695dba709551923
TrackRunny <trackrunny@protonmail.com>
2020-01-11 15:34:47 -0800

    Update python version

--- cogs/Information.py
+++ cogs/Information.py
@@ -106,7 +106,7 @@ class Information(commands.Cog):
         embed.add_field(name=f"• MEMBER Count:", inline=True, value=f":bust_in_silhouette: —  {users} users")
         embed.add_field(name=f"• GUILD Count:", inline=True, value=f":house: — {guilds} connected guilds")
         embed.add_field(name=f"• LIBRARY Version:", inline=True, value=f":gear: — Discord.py version 1.2.5")
-        embed.add_field(name=f"• PYTHON Version:", inline=True, value=f":snake:  — Python version 3.7.5")
+        embed.add_field(name=f"• PYTHON Version:", inline=True, value=f":snake:  — Python version 3.7.6")
         embed.set_footer(text=f"\n\nMade by TrackRunny#0001", icon_url=f"\n\nhttps://i.imgur.com/TiUqRH8.gif")
 
         await ctx.send(embed=embed)
